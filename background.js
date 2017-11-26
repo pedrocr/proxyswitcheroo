625c78faefe9c6626eb39f4c49f2c0b5cf774e74
Pedro Côrte-Real <pedro@pedrocr.net>
2017-11-26 19:39:10 +0000

    Toggle all UI in all tabs at once

--- background.js
+++ background.js
@@ -2,44 +2,37 @@ var currentProxy = false;
 
 function toggleProxy() {
   currentProxy = !currentProxy;
-  updateActiveTab();
-  console.log("Switched Proxy!");
+  refresh();
+}
+
+function refresh() {
+  var querying = browser.tabs.query({});
+  querying.then(doTabsUpdate);
+}
+
+function doTabsUpdate(tabs) {
+  for (let tab in tabs) {
+    browser.browserAction.setIcon({
+      path: currentProxy ? {
+        19: "icons/globe.png",
+        38: "icons/globe.png"
+      } : {
+        19: "icons/globe-disabled.png",
+        38: "icons/globe-disabled.png"
+      },
+      tabId: tab.id
+    });
+
+    browser.browserAction.setTitle({
+      // Screen readers can see the title
+      title: currentProxy ? 'No Proxy' : 'Proxy',
+      tabId: tab.id
+    });
+  }
 }
 
 browser.browserAction.onClicked.addListener(toggleProxy);
 
-function updateActiveTab(tabs) {
-  function updateTab(tabs) {
-    if (tabs[0]) {
-      currentTab = tabs[0];
-      browser.browserAction.setIcon({
-        path: currentProxy ? {
-          19: "icons/globe.png",
-          38: "icons/globe.png"
-        } : {
-          19: "icons/globe-disabled.png",
-          38: "icons/globe-disabled.png"
-        },
-        tabId: currentTab.id
-      });
-      browser.browserAction.setTitle({
-        // Screen readers can see the title
-        title: currentBookmark ? 'No Proxy' : 'Proxy',
-        tabId: currentTab.id
-      });
-    }
-  }
-
-  var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
-  gettingActiveTab.then(updateTab);
-}
-
-// listen to tab switching
-browser.tabs.onActivated.addListener(updateActiveTab);
-
-// listen for window switching
-browser.windows.onFocusChanged.addListener(updateActiveTab);
-
 // update when the extension loads initially
-updateActiveTab();
+refresh();
 
